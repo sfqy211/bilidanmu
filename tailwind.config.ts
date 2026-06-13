c89da5775a0434b3e71bc82fad63c935ad7cf171
sfqy211 <hangruizhang63@gmail.com>
2026-06-13 15:49:39 +0800

    style(config): 更新 B 站粉色色板及品牌色文档

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,20 @@ export default {
   theme: {
     extend: {
       colors: {
+        // B站官方粉色 (#FFC0CB) 色板
+        pink: {
+          50: "#fff0f3",
+          100: "#ffe0e6",
+          200: "#ffc0cb",
+          300: "#ffa0b0",
+          400: "#ff8096",
+          500: "#ff607c",
+          600: "#e84d66",
+          700: "#cc3a52",
+          800: "#b02a40",
+          900: "#94203a",
+          950: "#520c1c",
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
